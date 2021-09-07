995831737e5a5855e28a0c75f2ce3793c484f41c
Derek Jones <derekjones@asu.edu>
2021-09-07 01:40:30 -0400

    Made placeholder secret key

--- AoE_Openings/AoE_Openings/settings.py
+++ AoE_Openings/AoE_Openings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%ag-l$isq4w*!s9u5_0!!qh8+*v5ea9b93$027e5^)tpttje^'
+SECRET_KEY = 'FAKE_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
