6d1effcb9291071846fe34c62972334c75cae2a7
dhjpolymath <35887467+dhjpolymath@users.noreply.github.com>
2021-09-23 20:35:16 -0500

    added plot test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Civs from './components/civs'
 import Menu from './components/menu'
+import Test from './components/test'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
             <Switch>
               <Route path="/civ_stats" component={Civs}/>
               <Route path="/opening_stats" component={Civs}/>
+              <Route path="/test" component={Test}/>
             </Switch>
           </div>
         </BrowserRouter>
