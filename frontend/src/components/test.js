7f1194184121a3dc13a124cc022bb6369fcd2994
dhjpolymath <35887467+dhjpolymath@users.noreply.github.com>
2021-09-24 12:55:57 -0500

    removed unneeded import

--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Plot from 'react-plotly.js';
-import createPlotlyComponent from "react-plotly.js/factory";
 import * as graph from "../assets/plotly_vars.json";
 import * as figure from "../assets/plotly_vars.json";
 
