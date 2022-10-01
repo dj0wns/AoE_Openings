09687ef3ac6c0a1569a8919c4204282c7d903f1c
christopher@goodpairdays.com <christopher@goodpairdays.com>
2022-10-01 13:45:00 +0900

    Refactor app.js into functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import About from './components/about'
 import Home from './components/home'
 import Civs from './components/civs'
@@ -8,37 +8,25 @@ import Menu from './components/menu'
 import Advanced from './components/advanced'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-class App extends Component {
-  state = {
-    civilizations: []
-  }
-  componentDidMount() {
-  }
-  render () {
-    return (
-      <div>
-        <BrowserRouter>
-          <Menu/>
-          <div class="main-div">
-            <Switch>
-              <Route exact path="/" render={() => {
-                      return (
-                        <Redirect to="/home"/>
-                      )
-                  }}
-              />
-              <Route path="/about" component={About}/>
-              <Route path="/home" component={Home}/>
-              <Route path="/civ_stats" component={Civs}/>
-              <Route path="/opening_stats" component={OpeningWins}/>
-              <Route path="/opening_matchups" component={OpeningMatchups}/>
-              <Route path="/advanced" component={Advanced}/>
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Menu/>
+    <div class="main-div">
+      <Switch>
+        <Route 
+          render={() => { return <Redirect to="/home"/>; }}
+          path="/" 
+          exact 
+        />
+        <Route path="/home" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/civ_stats" component={Civs}/>
+        <Route path="/opening_stats" component={OpeningWins}/>
+        <Route path="/opening_matchups" component={OpeningMatchups}/>
+        <Route path="/advanced" component={Advanced}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
